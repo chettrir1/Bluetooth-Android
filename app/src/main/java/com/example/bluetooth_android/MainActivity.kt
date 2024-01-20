7b7555fb3c68ffe6bf92e0bcec882c1136500ab9
Raju <csta.puja@gmmail.com>
2024-01-19 20:57:39 -0500

    device screen added for showing scanned and paired devices

--- app/src/main/java/com/example/bluetooth_android/MainActivity.kt
+++ app/src/main/java/com/example/bluetooth_android/MainActivity.kt
@@ -1,16 +1,91 @@
 package com.example.bluetooth_android
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.bluetooth_android.presentation.BluetoothViewModel
+import com.example.bluetooth_android.presentation.components.DeviceScreen
 import com.example.bluetooth_android.ui.theme.BluetoothAndroidTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val bluetoothManager by lazy {
+        getSystemService(BluetoothManager::class.java)
+    }
+
+    private val bluetoothAdapter by lazy {
+        bluetoothManager?.adapter
+    }
+
+    private val isBluetoothEnabled: Boolean
+        get() = bluetoothAdapter?.isEnabled == true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BluetoothAndroidTheme {
+                val viewModel = hiltViewModel<BluetoothViewModel>()
+                val state by viewModel.state.collectAsState()
 
+                val enableBluetoothLauncher = registerForActivityResult(
+                    ActivityResultContracts.StartActivityForResult()
+                ) {
+
+                }
+
+                val permissionLauncher = registerForActivityResult(
+                    ActivityResultContracts.RequestMultiplePermissions()
+                ) { permission ->
+                    val canEnableBluetooth =
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                            permission[Manifest.permission.BLUETOOTH_CONNECT] == true
+                        } else {
+                            true
+                        }
+
+                    if (canEnableBluetooth && !isBluetoothEnabled) {
+                        enableBluetoothLauncher.launch(
+                            Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                        )
+
+                    }
+                }
+
+                LaunchedEffect(key1 = true) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                        permissionLauncher.launch(
+                            arrayOf(
+                                Manifest.permission.BLUETOOTH_SCAN,
+                                Manifest.permission.BLUETOOTH_CONNECT
+                            )
+                        )
+                    }
+                }
+
+                Surface(
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    DeviceScreen(
+                        state = state,
+                        onStartScan = viewModel::startScan,
+                        onStopScan = viewModel::stopScan
+                    )
+                }
             }
         }
     }
