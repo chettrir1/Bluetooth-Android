1d21429bd891449a32ad757e84a96186239a7fb3
Raju <chettrri.raju@gmail.com>
2024-01-30 11:57:21 -0500

    viewmodel updated with send message function

--- app/src/main/java/com/example/bluetooth_android/presentation/BluetoothUiState.kt
+++ app/src/main/java/com/example/bluetooth_android/presentation/BluetoothUiState.kt
@@ -1,11 +1,13 @@
 package com.example.bluetooth_android.presentation
 
 import com.example.bluetooth_android.domain.chat.BluetoothDevice
+import com.example.bluetooth_android.domain.chat.BluetoothMessage
 
 data class BluetoothUiState(
     val scannedDevices: List<BluetoothDevice> = emptyList(),
     val pairedDevices: List<BluetoothDevice> = emptyList(),
     val isConnected: Boolean = false,
     val isConnecting: Boolean = false,
-    val errorMessage: String? = null
+    val errorMessage: String? = null,
+    val message: List<BluetoothMessage> = emptyList()
 )
